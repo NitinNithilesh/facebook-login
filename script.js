ee9a905b9ee4efad8a2f5c115ee8cbfbeabfcea7
NitinNithilesh <nitin@yellow.ai>
2021-06-26 17:45:03 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
 $(document).ready(function () {
-
-  // add event listener on the login button
-
   $("#login").click(function () {
-
     facebookLogin();
-
-
   });
-
-  // add event listener on the logout button
-
-  // $("#logout").click(function () {
-
-  //   $("#logout").hide();
-  //   $("#login").show();
-  //   $("#status").empty();
-  //   facebookLogout();
-
-  // });
-
 
   function facebookLogin() {
     FB.getLoginStatus(function (response) {
@@ -29,34 +11,12 @@ $(document).ready(function () {
   }
 
   function statusChangeCallback(response) {
-    console.log('Enna da idhu pithalam: ', response);
-
     if (response && response.authResponse && response.authResponse.accessToken) {
-      var token = `<h6>TOKEN: ${response.authResponse.accessToken}</h6>`;
+      var token = `<h3>${response.authResponse.accessToken}</h6>`;
       $("#status").append(token);
     }
 
-    if (response.status === "connected") {
-      $("#login").hide();
-      $("#logout").show();
-      fetchUserProfile();
-    }
-    else {
-      // Logging the user to Facebook by a Dialog Window
-      facebookLoginByDialog();
-    }
-  }
-
-  function fetchUserProfile() {
-    // console.log('Welcome!  Fetching your information.... ');
-    // FB.api('/me?fields=id,name,email,gender,birthday', function (response) {
-    //   console.log(response);
-    //   console.log('Successful login for: ' + response.name);
-    //   var profile = `<h1>Welcome {response.name}<h1>
-    //      <h2>Your email is ${response.email}</h2>
-    //      <h3>Your Birthday is ${response.birthday}</h3>`;
-    //   $("#status").append(profile);
-    // });
+    facebookLoginByDialog();
   }
 
   function facebookLoginByDialog() {
@@ -66,11 +26,4 @@ $(document).ready(function () {
 
     }, { scope: 'public_profile,email, pages_show_list, pages_messaging, instagram_basic, instagram_manage_comments, instagram_manage_messages, pages_manage_metadata' });
   }
-
-  // logging out the user from Facebook
-  // function facebookLogout() {
-  //   FB.logout(function (response) {
-  //     statusChangeCallback(response);
-  //   });
-  // }
 });
